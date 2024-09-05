cfb2c83e2d0d6b151e11a25e16b900a544deeed6
Thamir-73 <Thamir.299@hotmail.com>
2024-09-05 12:36:25 +0300

    sdg

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
-        pathname: '/v0/b/moedti.appspot.com/**',
+        pathname: '/**',
       },
     ],
   },
